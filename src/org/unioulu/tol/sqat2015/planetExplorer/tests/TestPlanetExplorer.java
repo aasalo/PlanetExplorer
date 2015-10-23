d1c8e5fd32f0ce5b52a8bc5ed2031db5d11998cb
somename <someemail>
2015-10-23 13:20:55 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -105,4 +105,13 @@ public class TestPlanetExplorer {
 		
 		assertEquals("(0, 0), North", explorer.getLocation());
 	}
+	
+	@Test
+	public void testExplorerGoesAroundTurningLeft() {
+		PlanetExplorer explorer = new PlanetExplorer(3,3, "(1,0)");
+		
+		explorer.executeCommand("flflflfl");
+		
+		assertEquals("(0, 0), North", explorer.getLocation());
+	}
 }
